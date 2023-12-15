55bd5d340006a2704ffe8219cd34acf82285f0a6
marcoveiobrasil <marcoveioarts@gmail.com>
2023-12-15 17:36:17 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+# FLASK CONFIGURATION
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return 'Hello, World!'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
