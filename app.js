2e5dfde161bf1ad7c7051922bb1ef7bef01f6719
Marco Veio - Diadema <marcoveiocodes@google.com>
2024-02-24 12:24:46 +0100

    create post and view post, database connection

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
+import 'dotenv/config';
 import express from "express";
 import expressLayout from "express-ejs-layouts";
 import bodyParser from "body-parser";
+import sanitizeHtml from 'sanitize-html';
+
 
 // initiate app and ports
 const app = express();
 const port = 3000;
+
+// import db
+import db from './db/db.js';
 
 // middlewares
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -15,10 +21,79 @@ app.use(expressLayout);
 app.set('layout', './layouts/main');
 app.set('view engine', 'ejs');
 
+
 // GET - home
-app.get('/', (req, res) => {
-    res.render('index.ejs', { title: 'Página inicial'});
+app.get('/', async (req, res) => {
+    try {
+        // fetch all posts from database
+        const result = await db.query('SELECT * FROM posts');
+        const posts = result.rows;
+
+        // render index with posts
+        res.render('index.ejs', { 
+            title: 'Página inicial',
+            posts
+        });
+    } catch (error) {
+        console.error('Error fetching posts', error);
+        res.status(500).send('Error fetching posts');
+    }
 })
+
+// GET - add post page
+app.get('/create-post', (req, res) => {
+    res.render('create-post.ejs', { 
+        title: 'Novo post',
+        errorMessage: ''
+    })
+});
+
+// POST - create post
+app.post('/create-post', async(req, res) => {
+    const title = req.body["title"];
+    const intro = req.body["intro"];
+    const body = sanitizeHtml(req.body["body1"]);
+
+    try {
+        // Insert the post into the database
+        const result = await db.query('INSERT INTO posts (title, intro, body) VALUES ($1, $2, $3) RETURNING *', [title, intro, body ]);
+        const newPost = result.rows[0];
+    
+        // Redirect to the post page
+        res.redirect(`/post/${newPost.id}`);
+    } catch (error) {
+        console.error('Error creating post:', error);
+        // Set the error message
+        const errorMessage = 'Error creating post';
+        
+        // Redirect to the create-post page with the error message
+        res.render('create-post.ejs', { 
+            title: 'Novo post',
+            errorMessage 
+        });
+    }
+
+});
+
+// GET - view post
+app.get('/post/:postId', async (req, res) => {
+    const postId = req.params.postId;
+
+    try {
+        // Fetch the post from the database using the post ID
+        const result = await db.query('SELECT * FROM posts WHERE id = $1', [postId]);
+        const post = result.rows[0];
+    
+        // Render the post.ejs template with the post data
+        res.render('post.ejs', { 
+            post,
+            title: 'Post'
+         });
+      } catch (error) {
+        console.error('Error fetching post:', error);
+        res.status(500).send('Error fetching post');
+      }
+});
 
 // GET - about
 app.get('/about', (req, res) => {
---
+++ db/db.js
@@ -0,0 +1,15 @@
+import pg from "pg";
+
+// create DB
+const db = new pg.Client({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+  });
+
+// connect DB 
+db.connect();
+
+export default db
